7b83a3746db90c10960f9500b439e4a56d7eae83
vagrant <vagrant@homestead>
2018-11-02 12:55:04 +0000

    Added Code entity

---
+++ src/Entity/Code.php
@@ -0,0 +1,91 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping\Table;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @package App\Entity
+ *
+ * @Table(name="code")
+ * @ORM\Entity(repositoryClass="App\Repository\CodeRepository")
+ * @UniqueEntity("code")
+ */
+class Code
+{
+    /**
+     * @var int
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Id()
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(name="date", type="datetime")
+     * @Assert\NotBlank()
+     * @Assert\DateTime()
+     */
+    private $date;
+
+    /**
+     * @ORM\Column(name="code", type="string", length=10)
+     * @Assert\NotBlank()
+     */
+    private $code;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * @param mixed $date
+     */
+    public function setDate($date): void
+    {
+        $this->date = $date;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * @param mixed $code
+     */
+    public function setCode($code): void
+    {
+        $this->code = $code;
+    }
+
+}
